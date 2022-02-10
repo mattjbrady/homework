e56b17afba81fe2793d83e068101168ce1427cfb
Matt <mbrady@uccs.edu>
2022-02-10 18:45:43 +0000

    Set up invalid strategy exception for RPS class

--- hw-ruby-more/lib/rock_paper_scissors.rb
+++ hw-ruby-more/lib/rock_paper_scissors.rb
@@ -3,8 +3,12 @@ class RockPaperScissors
   # Exceptions this class can raise:
   class NoSuchStrategyError < StandardError ; end
 
+  def invalid_strategy?(player)
+    player[1].downcase.match(/r|p|s/) != 1
+  end
+
   def self.winner(player1, player2)
-    # your code here
+    raise NoSuchStrategyError, "Strategy must be one of R,P,S" if player1[1].downcase.match(/r|p|s/) != 1 || player2[1].downcase.match(/r|p|s/) != 1 
   end
 
   def self.tournament_winner(tournament)
